9110102abec180b7748809e292ab91499ad7e00d
Wonbin Jin <wbjin@umich.edu>
2025-11-16 23:04:51 -0500

    chore(format): Run formatter

--- scripts/format.sh
+++ scripts/format.sh
@@ -6,8 +6,10 @@ find src \( -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' \) -print0 | xargs 
 find itrace-cli \( -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' \) -print0 | xargs -0 -n1 echo
 find include \( -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' \) -print0 | xargs -0 -n1 echo
 find examples \( -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' \) -print0 | xargs -0 -n1 echo
+find itrace-tui \( -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' \) -print0 | xargs -0 -n1 echo
 
 find src \( -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' \) -print0 | xargs -0 clang-format -i
 find itrace-cli \( -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' \) -print0 | xargs -0 clang-format -i
 find include \( -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' \) -print0 | xargs -0 clang-format -i
 find examples \( -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' \) -print0 | xargs -0 clang-format -i
+find itrace-tui \( -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' \) -print0 | xargs -0 clang-format -i
